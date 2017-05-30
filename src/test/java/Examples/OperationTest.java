add9ce87a2bdb1086ca03cf32c641759cb274cd3
Johan Moreau <johan.moreau@gmail.com>
2017-05-30 08:55:10 +0200

    Add error in unittest

--- src/test/java/Examples/OperationTest.java
+++ src/test/java/Examples/OperationTest.java
@@ -21,4 +21,13 @@ public class OperationTest {
 		
 	}
 	
+	@Test
+	public void additionTest() {
+		int a = 2;
+		int b = 5;
+		int expectedResult = Operation.addition(a,b);
+		
+		assertEquals(8,expectedResult);
+		
+	}
 }
